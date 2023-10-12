1ea402dec54fd897edb56cac0fee88af23768608
Aktan Sanhal <aktansanhal@gmail.com>
2023-10-12 06:11:15 +0300

    deploy

--- voyage-slider/src/App.js
+++ voyage-slider/src/App.js
@@ -2,33 +2,27 @@ import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
-import useSound from 'use-sound';
+
 
 function App() {
-  const [play] = useSound(  '/sounds/music.mp3', { volume: 0.3});
-  
-  useEffect(() => {
-
-    play();
-  
-  }, [play]);
+ 
 
   return (
-    
-      <Router>
+    <BrowserRouter>
+     
         <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
-        </Switch>
-      </Router>
-   
+        <Routes>
+          <Route index path='/' exact component={Home} />
+          <Route exact  path='/services' component={Services} />
+          <Route exact  path='/products' component={Products} />
+          <Route exact  path='/sign-up' component={SignUp} />
+        </Routes>
+  
+      </BrowserRouter>
   );
 }
 
